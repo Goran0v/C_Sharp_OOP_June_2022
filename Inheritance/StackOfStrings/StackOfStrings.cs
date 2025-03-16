f69d754c4eda9d4f5215498ae9c5988cdd244bcc
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-03-16 18:13:30 +0200

    Add the exercises

---
+++ Inheritance/StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public StackOfStrings(IEnumerable<string> stack)
+        {
+            this.AddRange(stack);
+        }
+
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddRange(IEnumerable<string> stack)
+        {
+            foreach (var str in stack)
+            {
+                this.Push(str);
+            }
+        }
+    }
+}
